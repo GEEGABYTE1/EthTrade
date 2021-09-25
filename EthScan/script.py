b49dab4b132b2166fbf5fe2b5249fa401415e614
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-09-25 12:58:44 -0400

    Formatting

--- EthScan/script.py
+++ EthScan/script.py
@@ -4,19 +4,24 @@ import time
 class Script:
 
     def intro(self):
-        print("\t*5")
-        print("To view the blockchain, here are the certain commands: ")
-        time.sleep(0.2)
+        print("\t"*5)
+        print("To view the blockchain, here are the commands: ")
+        print('\n')
+        time.sleep(0.5)
         print("/connect_chain")
         print("-"*15)
-        time.sleep(0.2)
+        time.sleep(0.5)
         print("/view_latest_block")
         print("-"*15)
-        time.sleep(0.2)
+        time.sleep(0.5)
         print("/view_multiple_blocks")
         print("-"*15)
-        time.sleep(0.2)
+        time.sleep(0.5)
         print("/view_transaction_block")
         print("-"*15)
-        time.sleep(0.2)
+        time.sleep(0.5)
+        print("\t"*5)
+
+
+
 
