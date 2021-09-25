efbc20b90189cca197b9fe3e16ba0209512e7e88
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-09-25 14:23:53 -0400

    Partioned Function

--- EthScan/script.py
+++ EthScan/script.py
@@ -52,38 +52,8 @@ class Script:
                     print("Connection Successful")               
 
             elif prompt == '/view_latest_block':
-                try:
-                    latest_block = ether_base.get_latest_block()
-                    time.sleep(0.2)
-                    
-                    retrival_options = []
-                    for heading, information in latest_block.items():
-                        print('{}:'.format(heading))
-                        if type(information) != str and type(information) != int:
-                            information_formatted = ether_base.web3.toHex(information)
-                            information = information_formatted
-                        print('{}'.format(information))
-                        print(type(information))
-                        print('-'*24)  
-                        retrival_options.append(heading) 
-
-                    prompt2 = str(input("Would you like to retrieve something? type(y/n): "))
-                    if prompt2 == 'y':
-                        info = str(input('What would you like to retrieve? '))
-                        info = info.lower()
-                        info = info.strip(" ")
-                        retrieved_data = latest_block.get(info, None)
-                        if retrival_data == None:
-                            print('Data not found')
-                            print('Read documentation for more information')
-                        else:
-                            self.saved_user_retrieves[info + str(self.counter)] = retrieved_data
-                            print("Information succesfully stored ")
-        
-                except AttributeError:
-                    print("Looks like you have connected to the Ethereum Chain yet")
-                    time.sleep(0.5)
-                    print('Type /connect_chain to do so!')  
+                latest_block = ether_base.get_latest_block()
+                self.viewing_block_information(latest_block)
 
 
             elif prompt == '/view_multiple_blocks':
@@ -100,6 +70,48 @@ class Script:
                 pass
 
             self.counter += 1
+
+            
+    def viewing_block_information(self, block):
+        try:
+            time.sleep(0.2)
+            retrival_options = []
+            for heading, information in block.items():
+                print('{}:'.format(heading))
+                if type(information) != str and type(information) != int and type(information) != list:
+                    information_formatted = ether_base.web3.toHex(information)
+                    information = information_formatted
+                print('{}'.format(information))
+                #print(type(information))
+                print('-'*24)  
+                retrival_options.append(heading) 
+
+            time.sleep(1)
+            prompt2 = str(input("Would you like to retrieve something? type(y/n): "))
+            if prompt2 == 'y':
+                
+                time.sleep(0.4)
+                print()
+                for heading in block.keys():
+                    print(heading)
+                    print('-'*24)
+
+                info = str(input('What would you like to retrieve? '))
+                info = info.lower()
+                info = info.strip(" ")
+                retrieved_data = block.get(info, None)
+                if retrieved_data == None:
+                    print('Data not found')
+                    print('Read documentation for more information')
+                else:
+                    self.saved_user_retrieves[info + str(self.counter)] = retrieved_data
+                    print("Information succesfully stored ")
+
+        except AttributeError:
+            print("Looks like you have connected to the Ethereum Chain yet")
+            time.sleep(0.5)
+            print('Type /connect_chain to do so!')  
+
 
 
 
