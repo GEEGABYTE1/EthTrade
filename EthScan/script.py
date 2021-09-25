7d8dfc3729e195a537c7b610a5d34c5a9675587e
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-09-25 12:57:13 -0400

    Prompt Graphics

---
+++ EthScan/script.py
@@ -0,0 +1,22 @@
+import time
+
+
+class Script:
+
+    def intro(self):
+        print("\t*5")
+        print("To view the blockchain, here are the certain commands: ")
+        time.sleep(0.2)
+        print("/connect_chain")
+        print("-"*15)
+        time.sleep(0.2)
+        print("/view_latest_block")
+        print("-"*15)
+        time.sleep(0.2)
+        print("/view_multiple_blocks")
+        print("-"*15)
+        time.sleep(0.2)
+        print("/view_transaction_block")
+        print("-"*15)
+        time.sleep(0.2)
+
