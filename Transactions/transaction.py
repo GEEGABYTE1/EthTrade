1e744567e0e09719e25ab4d145b8dd20f175b3aa
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-10-03 22:53:38 -0400

    Base TransactionBackend Commands

--- Transactions/transaction.py
+++ Transactions/transaction.py
@@ -16,3 +16,32 @@ class Transaction:
             return True 
         else:
             return False
+
+
+    def create_transaction(self, account_1_hash, account_2_hash, private_key):
+        transaction_dictionary = {'nonce': None, 'to': None, 'value':None, 'gas':200000, 'gasPrice':self.web3.toWei('50', 'gwei')}
+        nonce = self.web3.eth.getTransactionCount(account_1_hash)
+        value = int(input("Please enter the amount (*In WEI*) you would like to send: "))
+        information = [nonce, account_2_hash, value]
+        for header, data in transaction_dictionary.items():
+            if len(information) == 0:
+                break
+            else:
+                info = information.pop(0)
+                transaction_dictionary[header] = info
+
+        return transaction_dictionary
+
+    def sign_transaction(self, transaction, private_key):
+        signed_trans = self.web3.eth.account.signTransaction(transaction, private_key)
+        signed_hash = self.web3.eth.sendRawTransaction(signed_trans.rawTransaction)
+        signed_hash = self.web3.toHex(signed_hash)
+        return signed_hash
+
+        
+        
+
+            
+
+        
+        
