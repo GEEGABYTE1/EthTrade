7902e1ad70376b8a035fed0345d9264c35f405ca
Jaival Patel <68164588+GEEGABYTE1@users.noreply.github.com>
2021-10-30 17:11:15 -0400

    bug fixes

--- EthScan/ether_scan.py
+++ EthScan/ether_scan.py
@@ -1,4 +1,5 @@
 from web3 import Web3
+import hashlib
 
 class Scan:
     infura_url = None 
@@ -27,8 +28,8 @@ class Scan:
     def get_multiple_blocks(self, number):
         blocks = []
         for block in range(0, number):
-            current_block = self.web3.eth.getBlock(latest - block)
-            bocks.append(current_block)
+            current_block = self.web3.eth.getBlock('latest')
+            blocks.append(current_block)
     
         return blocks 
 
@@ -36,7 +37,17 @@ class Scan:
         transaction_details = self.web3.eth.getTransactionByBlock(block_hash, trans_number)
         return transaction_details
 
+    def mining(self, block_num, from_account_hash, transaction_hash, previous_hash, difficulty):
+        nonce_limit = self.web3.eth.getTransactionCount(from_account_hash)
+        for nonce in range(nonce_limit):
+            base_text = str(block_num) + str(transaction_hash) + str(previous_hash) + str(nonce)
+            hash_try = haslib.sha256(base_text.encode()).hexdigest()
+            string = '0' * difficulty
+            if hash_try[:difficulty] == string:
+                print("Found hash with nonce: {}".format(nonce))
+                return hash_try 
+
+        return None        
 
 
 ether_base = Scan()
-
